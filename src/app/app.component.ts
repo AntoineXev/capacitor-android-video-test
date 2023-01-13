eb124e4d3667bf56d7ddb249ac0feaa84404b187
Antoine <antoine.hervet@xevlabs.com>
2023-01-13 15:44:40 +0100

    Added video recording

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import { Plugins } from '@capacitor/core';
+import { VideoRecorderCamera, VideoRecorderPreviewFrame, VideoRecorder } from '@teamhive/capacitor-video-recorder';
+
+const ratioWidth = 9;
+const ratioHeight = 16;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'capacitor-video-test';
+  public videoRecorderLaunched = false;
+  videoRecorderOptions: VideoRecorderPreviewFrame = {
+    id: 'video-record',
+    stackPosition: 'back',
+    width: window.screen.width / ratioWidth > window.screen.height / ratioHeight ? Math.floor(window.screen.height / ratioHeight * ratioWidth) : window.screen.width,
+    height: window.screen.height / ratioHeight > window.screen.width / ratioWidth ? Math.floor(window.screen.width / ratioWidth * ratioHeight) : window.screen.height,
+    x: 0,
+    y: 0,
+    borderRadius: 0
+  };
+
+  constructor(
+          ) {
+  }
+
+  recordVideo() {
+    this.videoRecorderLaunched = true;
+    VideoRecorder.initialize({
+      camera: VideoRecorderCamera.BACK,
+      previewFrames: [this.videoRecorderOptions]
+    }).then(_ => {
+      console.log("preview")
+    })
+  }
 }
